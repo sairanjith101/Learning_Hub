add877eed1582e06e4916c0c8875eb35b41af97f
sairanjith101 <sairanjith101@gmail.com>
2025-06-24 19:52:43 +0530

    add

--- Django/dj_train/Models_and_ORM/test.py
+++ Django/dj_train/Models_and_ORM/test.py
@@ -1,34 +1,5 @@
-from myapp.models import Student
+from django.db.models import Count,Author
 
-# create
-
-student = Student(name = 'Ravi', age = 20)
-student.save()
-
-student = Student.objects.create(name='Ravi', age=20)
-
-# read
-
-student = Student.objects.all()
-
-student = Student.objects.get(id=id)
-
-student = Student.objects.filter(age__gte=8)
-
-student = Student.objects.order_by('name')
-
-# update
-
-student = Student.objects.get(id=id)
-student.name = "Raj"
-student.save()
-
-student = Student.objects.filter(id=id).update(name="Raj")
-
-
-# delete
-
-student = Student.objects.get(id=id)
-student.delete()
-
-student = Student.objects.filter(id=id).delete()
+authors = Author.objects.annotate(book_count = Count('book'))
+for author in authors:
+    print(author.name, author.book)
