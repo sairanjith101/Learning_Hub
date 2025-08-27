b3ddb70bb77a477a8c8819c6497c5e4b9a8f9742
sairanjith101 <sairanjith101@gmail.com>
2025-08-27 13:16:42 +0530

    learn about DRF views logics

--- Project/Nexora/myapi_project/test.py
+++ Project/Nexora/myapi_project/test.py
@@ -1,30 +1,18 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
-from rest_framework import serializers
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .Serializer import RegisterSerializer, ProfileSerializer
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, require=True)
-    role = serializers.ChoiceField(choices=User.Roles.choices, default = User.Roles.CUSTOMER)
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'email', 'first_name', 'last_name', 'address', 'phone', 'role')
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    def validate_password(self, value):
-        validate_password(value)
-        return value
-    
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-    
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'address', 'role')
-        read_only_fields = ('id', 'username', 'email', 'role')
+    def get_object(self):
+        return self.request.user
