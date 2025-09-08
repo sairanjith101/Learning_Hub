f141bebdd70db3236fa1de7b9884034bd08ed3f1
sairanjith101 <sairanjith101@gmail.com>
2025-09-08 12:42:56 +0530

    cart

--- Project/Nexora/myapi_project/test.py
+++ Project/Nexora/myapi_project/test.py
@@ -1,10 +1,59 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from cart.models import Cart, CartItem
+from cart.serializers import CartSerializer, CartItemSerializer
+from products.models import Product
+
+class CartViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        cart,_ = Cart.objects.get_or_create(user=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+
+    def create(self, request):
+        cart,_ = Cart.objects.get_or_create(user=request.user)
+        serializer = CartItemSerializer(cart)
+        if serializer.is_valid():
+            product = serializer.validated_data['product']
+            quantity = serializer.validated_data['quantity']
+
+            item, created = CartItem.objects.get_or_create(data=request.data)
+            if not created:
+                item.quantity += quantity
+            else:
+                item.quantity = quantity
+            item.save()
+            return Response(CartItemSerializer(cart).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        cart,_ = Cart.objects.get_or_create(user=request.user)
+        try:
+            item = cart.items.get(pk=pk)
+        except CartItem.DoesNotExist:
+            return Response({"error: Item not found"}, status=404)
+        
+        serializer = CartItemSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(CartSerializer(cart).data)
+        return Response(serializer.errors, status=400)
+    
+    def destroy(self, request, pk=None):
+        cart,_ = Cart.objects.get_or_create(user=request.user)
+        try:
+            item = cart.items.get(pk=pk)
+            item.delete()
+        except CartItem.DoesNotExist:
+            return Response({"error: Item not found"}, status=404)
+        return Response(CartSerializer(cart).data)
+
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, BrandViewSet, ProductViewSet
+from django.urls import path
+from cart.views import CartViewSet
 
 router = DefaultRouter()
-
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('brand', BrandViewSet, basename='brand')
-router.register('product', ProductViewSet, basename='product')
-
-urlpatterns = router.urls
+router.register('cart', CartViewSet, basename='cart')
