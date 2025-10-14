c9478e258eb88959f6b5e12579deb35e85ae2811
sairanjith101 <sairanjith101@gmail.com>
2025-10-14 13:00:31 +0530

    leetcode

--- Python/py_train/GFG/Easy/merge.py
+++ Python/py_train/GFG/Easy/merge.py
@@ -2,10 +2,12 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
+        # Start from the end of both arrays
+        i = m - 1  # last valid element in nums1
+        j = n - 1  # last element in nums2
+        k = m + n - 1  # last position in nums1
 
+        # Merge from back to front
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -14,15 +16,15 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-        
+
+        # If any elements left in nums2, copy them
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
         
         return nums1
-
-
+        
 
 nums1 = [1,2,3,0,0,0]
 m = 3
